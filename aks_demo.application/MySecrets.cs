617e7b157250184cfe1c819c9cafba0330b28a85
Glen Colby <gcolby@healthwise.org>
2021-04-06 11:45:53 -0600

    Initial Commit

---
+++ aks_demo.application/MySecrets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace aks_demo.application
+{
+    public class MySecrets
+    {
+        public string myNewSecret { get; set; }
+        public string myOtherNewSecret { get; set; }
+    }
+}
---
+++ aks_demo.application/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aks_demo.application.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        public Dictionary<string, string> appSettings { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            appSettings = new Dictionary<string, string>();
+        }
+
+        public void OnGet()
+        {
+            var mySecrets = new MySecrets();
+            _configuration.GetSection("MySecrets").Bind(mySecrets);
+            appSettings.Add("My new secret", mySecrets.myNewSecret);
+            appSettings.Add("My other new secret", mySecrets.myOtherNewSecret);
+
+        }
+    }
+}
